b3f4a45646bb7f27192e165ff9e5c6824042e09d
learnwithusmansagheer <apnausmansagheer@gmail.com>
2024-11-13 18:07:48 +0500

    feat: made welcomescreen design

--- android/app/src/components/boxwithHeadingandImage/BoxwithHeadingandImage.jsx
+++ android/app/src/components/boxwithHeadingandImage/BoxwithHeadingandImage.jsx
@@ -17,7 +17,7 @@ const styles = StyleSheet.create({
   container: {
     display: 'flex',
     flexDirection: 'column',
-    height: 200,
+
     width: '100%',
     justifyContent: 'center',
     alignItems: 'center',
