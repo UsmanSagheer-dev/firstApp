e00ffb88d5c921cc8a7f2a5cf59ddbb2849e7ae8
learnwithusmansagheer <apnausmansagheer@gmail.com>
2024-11-13 12:21:19 +0500

    feat: we change statusBaar

--- App.tsx
+++ App.tsx
@@ -1,53 +1,54 @@
 import React from 'react';
-import {View, Text, StyleSheet, ScrollView} from 'react-native';
-import FirstCard from './android/app/src/components/firstCard/FirstCard';
+import { View, Text, StyleSheet, ScrollView, StatusBar } from 'react-native';
 
 function App() {
   return (
-    <ScrollView style={styles.container}>
-      <View style={[styles.box]}>
-        <View style={[styles.box, {flex: 1, backgroundColor: 'red'}]}>
-          <Text style={styles.text}>Flex 1</Text>
+    <>
+
+      <StatusBar backgroundColor="#4B0082" barStyle="light-content" />
+
+      <ScrollView style={styles.container}>
+        <View style={[styles.box]}>
+          <View style={[styles.box, { flex: 1, backgroundColor: 'red' }]}>
+            <Text style={styles.text}>Flex 1</Text>
+          </View>
+          <View style={[styles.box, { flex: 1, backgroundColor: 'green' }]}>
+            <Text style={styles.text}>Flex 2</Text>
+          </View>
         </View>
-        <View style={[styles.box, {flex: 1, backgroundColor: 'green'}]}>
-          <Text style={styles.text}>Flex 2</Text>
+        <View style={[styles.box]}>
+          <View style={[styles.box, { flex: 2, backgroundColor: 'purple' }]}>
+            <Text style={styles.text}>Flex 3</Text>
+          </View>
+          <View style={[styles.box, { flex: 2, backgroundColor: 'orange' }]}>
+            <Text style={styles.text}>Flex 4</Text>
+          </View>
         </View>
-      </View>
-      <View style={[styles.box]}>
-        <View style={[styles.box, {flex: 2, backgroundColor: 'purple'}]}>
-          <Text style={styles.text}>Flex 3</Text>
+        <View style={[styles.box]}>
+          <View style={[styles.box, { flex: 2, backgroundColor: 'blue' }]}>
+            <Text style={styles.text}>Flex 5</Text>
+          </View>
+          <View style={[styles.box, { flex: 2, backgroundColor: 'gray' }]}>
+            <Text style={styles.text}>Flex 6</Text>
+          </View>
         </View>
-        <View style={[styles.box, {flex: 2, backgroundColor: 'orange'}]}>
-          <Text style={styles.text}>Flex 4</Text>
+        <View style={[styles.box]}>
+          <View style={[styles.box, { flex: 2, backgroundColor: 'purple' }]}>
+            <Text style={styles.text}>Flex 3</Text>
+          </View>
+          <View style={[styles.box, { flex: 2, backgroundColor: 'orange' }]}>
+            <Text style={styles.text}>Flex 4</Text>
+          </View>
         </View>
-      </View>
-      <View style={[styles.box]}>
-        <View style={[styles.box, {flex: 2, backgroundColor: 'blue'}]}>
-          <Text style={styles.text}>Flex 5</Text>
-        </View>
-        <View style={[styles.box, {flex: 2, backgroundColor: 'gray'}]}>
-          <Text style={styles.text}>Flex 6</Text>
-        </View>
-      </View>
-      <View style={[styles.box]}>
-        <View style={[styles.box, {flex: 2, backgroundColor: 'purple'}]}>
-          <Text style={styles.text}>Flex 3</Text>
-        </View>
-        <View style={[styles.box, {flex: 2, backgroundColor: 'orange'}]}>
-          <Text style={styles.text}>Flex 4</Text>
-        </View>
-      </View>
-      <View>
-        <FirstCard/>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20, 
+    padding: 20,
   },
   box: {
     flexDirection: 'row',
@@ -57,7 +58,7 @@ const styles = StyleSheet.create({
     height: 200,
     borderRadius: 20,
     gap: 10,
-    marginBottom: 20, 
+    marginBottom: 20,
   },
   text: {
     color: 'white',
