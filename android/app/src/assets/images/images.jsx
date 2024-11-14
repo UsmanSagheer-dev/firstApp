83974d1e80cb54b5a571009a74564be8c5e7f764
learnwithusmansagheer <apnausmansagheer@gmail.com>
2024-11-14 18:45:53 +0500

    feat: update home-screen

--- android/app/src/assets/images/images.jsx
+++ android/app/src/assets/images/images.jsx
@@ -1,9 +1,13 @@
 
 import welcomeImage from "../../assets/images/welcomeimage.jpg"
 import bigcart from "../../assets/images/bigcart.png"
+import filter from '../../assets/images/filter.png'
+import searchIcon from '../images/searchIcon.png'
 export const Images={
 
     WELCOME_IMAGE:welcomeImage,
     BIG_CART:bigcart,
+    FILTER:filter,
+    SEARCH_ICON:searchIcon,
 }
 
